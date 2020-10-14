a9a10e526e963d98f0090a51ef42b4051bf6470c
Richard Lloyd <relloyd@gmail.com>
2020-10-14 12:29:05 +0100

    --wip-- [skip ci]

---
+++ providers/aws/dev/ec2.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "ec2_instance_role" {
+  name = "ec2_instance_role"
+
+  assume_role_policy = templatefile("${path.module}/ec2-policy.json.tpl", {
+    bucket_arn = aws_s3_bucket.bucket.arn
+  })
+
+  tags = {
+    project = "matchesfashion"
+  }
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_instance_role.name
+
+  tags = {
+    project = "matchesfashion"
+  }
+}
+
